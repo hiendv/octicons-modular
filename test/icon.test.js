b73079e3da139705c5ffe77e04c0138541de7ddf
Andrei Pavlenko <andpawlenko@gmail.com>
2017-10-06 16:21:39 +0300

    test each icon build

--- test/icon.test.js
+++ test/icon.test.js
@@ -1,44 +1,48 @@
-describe('an icon', () => {
-  beforeEach(() => {
-    jest.resetModules()
-  })
+import octicons from './fixtures/icons'
 
-  test('calls the attrs method once when calling the svg method', () => {
-    var zap = require('../lib/icons/zap.js')
-    zap.attrs = jest.fn()
-    zap.svg()
+octicons.forEach(name => {
+  describe(`${name} icon`, () => {
+    beforeEach(() => {
+      jest.resetModules()
+    })
 
-    expect(zap.attrs).toHaveBeenCalled()
-  })
+    test('calls the attrs method once when calling the svg method', () => {
+      var icon = require(`../lib/icons/${name}.js`)
+      icon.attrs = jest.fn()
+      icon.svg()
 
-  test('calls the attrs method with the same parameters once when calling the svg method', () => {
-    var zap = require('../lib/icons/zap.js')
-    const options = {
-      key: 'value'
-    }
+      expect(icon.attrs).toHaveBeenCalled()
+    })
 
-    zap.attrs = jest.fn()
-    zap.svg(options)
+    test('calls the attrs method with the same parameters once when calling the svg method', () => {
+      var icon = require(`../lib/icons/${name}.js`)
+      const options = {
+        key: 'value'
+      }
 
-    expect(zap.attrs).toHaveBeenCalledWith(options)
-  })
+      icon.attrs = jest.fn()
+      icon.svg(options)
 
-  test('calls the attrsFormat method once when calling the svg method', () => {
-    var zap = require('../lib/icons/zap.js')
-    zap.attrsFormat = jest.fn()
-    zap.svg()
+      expect(icon.attrs).toHaveBeenCalledWith(options)
+    })
 
-    expect(zap.attrsFormat).toHaveBeenCalled()
-  })
+    test('calls the attrsFormat method once when calling the svg method', () => {
+      var icon = require(`../lib/icons/${name}.js`)
+      icon.attrsFormat = jest.fn()
+      icon.svg()
 
-  test('calls the attrs method and receive the value returned from attrsFormat method once when calling the svg method', () => {
-    var zap = require('../lib/icons/zap.js')
-    zap.attrsFormat = jest.fn()
-      .mockReturnValue('value')
+      expect(icon.attrsFormat).toHaveBeenCalled()
+    })
 
-    zap.svg()
+    test('calls the attrs method and receive the value returned from attrsFormat method once when calling the svg method', () => {
+      var icon = require(`../lib/icons/${name}.js`)
+      icon.attrsFormat = jest.fn()
+        .mockReturnValue('value')
 
-    expect(zap.attrsFormat).toHaveBeenCalled()
-    expect(zap.attrs()).toBe('value')
+      icon.svg()
+
+      expect(icon.attrsFormat).toHaveBeenCalled()
+      expect(icon.attrs()).toBe('value')
+    })
   })
 })
