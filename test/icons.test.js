80042794f11b0d02b3e7130d264f8db509530613
Andrei Pavlenko <andpawlenko@gmail.com>
2017-10-10 13:43:56 +0300

    refactored icons tests

--- test/icons.test.js
+++ test/icons.test.js
@@ -41,8 +41,7 @@ icons.forEach(name => {
         expect(icon.svg({ scale })).toBe(`<svg version="1.1" width="${actualWidth}" height="${actualHeight}" viewBox="0 0 ${octicon.width} ${octicon.height}" class="octicon octicon-${name}" aria-hidden="true" >${octicon.path}</svg>`)
       })
 
-      test('returns a svg which scales to a fractional number', () => {
-        expect(watch.svg({ scale: 1.2345 })).toBe('<svg version="1.1" width="14.81" height="19.75" viewBox="0 0 12 16" class="octicon octicon-watch" aria-hidden="true" ><path fill-rule="evenodd" d="M6 8h2v1H5V5h1v3zm6 0c0 2.22-1.2 4.16-3 5.19V15c0 .55-.45 1-1 1H4c-.55 0-1-.45-1-1v-1.81C1.2 12.16 0 10.22 0 8s1.2-4.16 3-5.19V1c0-.55.45-1 1-1h4c.55 0 1 .45 1 1v1.81c1.8 1.03 3 2.97 3 5.19zm-1 0c0-2.77-2.23-5-5-5S1 5.23 1 8s2.23 5 5 5 5-2.23 5-5z"/></svg>')
+        expect(icon.svg({ scale })).toBe(`<svg version="1.1" width="${actualWidth}" height="${actualHeight}" viewBox="0 0 ${octicon.width} ${octicon.height}" class="octicon octicon-${name}" aria-hidden="true" >${octicon.path}</svg>`)
       })
 
       test(`returns a svg with an addional class`, () => {
