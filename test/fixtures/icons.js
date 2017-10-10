5e6445a1350a9f77d858095abfe6097191f6501c
hiendv <neo.go.vn@gmail.com>
2017-10-10 12:22:53 +0700

    updates test fixtures

--- test/fixtures/icons.js
+++ test/fixtures/icons.js
@@ -1,13 +1,3 @@
 const octicons = require('octicons/build/data')
-
-const icons = Object.keys(octicons)
-const paths = icons.reduce((paths, name) => {
-  return (paths[name] = octicons[name].path) && paths
-}, {})
-
-export {
-  icons,
-  paths
-}
-
-export default icons
+export const icons = Object.keys(octicons)
+export default octicons
