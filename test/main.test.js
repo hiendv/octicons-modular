1635a03bb7b07a072ad9b491b84718faa014e93c
hiendv <neo.go.vn@gmail.com>
2018-03-05 14:46:33 +0700

    update main test

--- test/main.test.js
+++ test/main.test.js
@@ -1,9 +1,29 @@
+/* eslint-disable */
 import icons from '../src/data.js'
 import { t } from './utils.js'
 
 describe('CommonJS library', () => {
-  const Octicons = require('../')
-  icons.forEach(icon => {
-    t(Octicons[icon.camelName], icon)
+  describe('with built icons', () => {
+    const Octicons = require('../')
+    icons.forEach(icon => {
+      t(Octicons[icon.camelName], icon)
+    })
+  })
+
+  describe('with a fake icon', () => {
+    jest.resetModules()
+    jest.doMock(`../lib/icons/alert.js`, () => {
+      return {
+        default: 'something else'
+      }
+    })
+
+    const Octicons = require('../')
+    test(`replaces the alert icon to the fake one`, () => {
+      expect(Octicons.alert).toBe('something else')
+    })
+
+    jest.unmock(`../lib/icons/alert.js`)
+    jest.resetModules()
   })
 })
