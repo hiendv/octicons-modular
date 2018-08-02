76d665c3c1ae562ecb5cef85fbbbdc24dd7a798a
hiendv <hien.dv.neo@gmail.com>
2018-08-02 13:45:56 +0700

    adapters: add ES tests

---
+++ packages/octicons-react/test/main.es.test.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import Renderer from 'react-test-renderer'
+import Octicon from '../lib/main.es.js'
+
+describe('Octicon component', () => {
+  const zap = require('octicons-modular/lib/icons/zap')
+
+  test('invalid', () => {
+    const component = Renderer.create(
+      <Octicon icon={{ svg () {} }} />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toBeFalsy()
+  })
+
+  test('valid', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('scalable', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} scale={2} />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('scalable with 0', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} scale={0} />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('scalable with fractional values', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} scale={1.2345} />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('accepts an additional class', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} className="an-additional-class" />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('ignores invalid additional classes', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} className={null} />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('accepts an label', () => {
+    const component = Renderer.create(
+      <Octicon icon={zap} label="a label" />
+    )
+
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ packages/octicons-vue/test/main.es.test.js
@@ -0,0 +1,88 @@
+import { mount } from '@vue/test-utils'
+import Octicon from '../lib/main.es.js'
+
+describe('Octicon component', () => {
+  const icon = require('octicons-modular/lib/icons/zap')
+
+  test('invalid', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon: {
+          svg () {}
+        } }
+      }
+    })
+
+    expect(wrapper.html()).toBeFalsy()
+  })
+
+  test('valid', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('scalable', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon, scale: 2 }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('scalable with 0', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon, scale: 0 }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('scalable with fractional values', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon, scale: 1.2345 }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('accepts an additional class', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon, className: 'an-additional-class' }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('ignores invalid additional classes', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon, className: null }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('accepts an label', () => {
+    const wrapper = mount(Octicon, {
+      context: {
+        props: { icon, label: 'label' }
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
