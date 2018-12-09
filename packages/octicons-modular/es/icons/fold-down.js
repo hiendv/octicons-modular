bd19249854cc658408920a90efd4314c28923a5d
hiendv <hien.dv.neo@gmail.com>
2018-12-10 01:30:25 +0700

    core: update deps

---
+++ packages/octicons-modular/es/icons/fold-down.js
@@ -0,0 +1,6 @@
+import factory from '../octicon.js';
+
+// This is an auto-generated ES2015 icon from the modularize script. Please do not modify this file.
+var foldDown = factory('fold-down', 14, 16, {"fill-rule":"evenodd","d":"M4 11l3 3 3-3H8V5H6v6H4zm-4 0c0 .55.45 1 1 1h2.5l-1-1h-1l2-2H5V8H3.5l-2-2H5V5H1c-.55 0-1 .45-1 1l2.5 2.5L0 11zm10.5-2H9V8h1.5l2-2H9V5h4c.55 0 1 .45 1 1l-2.5 2.5L14 11c0 .55-.45 1-1 1h-2.5l1-1h1l-2-2z"}, ["unfold","hide","collapse","down"]);
+
+export default foldDown;
