3c47a1bc40cfd49c9e424913de6044a9365d9faa
hiendv <hien.dv.neo@gmail.com>
2019-01-11 14:13:24 +0700

    feat(core): update octicons@8.3.0

---
+++ packages/octicons-modular/es/icons/request-changes.js
@@ -0,0 +1,6 @@
+import factory from '../octicon.js';
+
+// This is an auto-generated ES2015 icon from the modularize script. Please do not modify this file.
+var requestChanges = factory('request-changes', 16, 15, {"fill-rule":"evenodd","d":"M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H7.5L4 15.5V12H1a1 1 0 0 1-1-1V1zm1 0v10h4v2l2-2h8V1H1zm7.5 3h2v1h-2v2h-1V5h-2V4h2V2h1v2zm2 5h-5V8h5v1z"}, ["diff","changes","request"]);
+
+export default requestChanges;
