121e37622c9fdc0cd75c385493d160b8cd0071aa
hiendv <hien.dv.neo@gmail.com>
2019-09-12 18:00:03 +0700

    fix: resolve a regression at 5be4c3b with rollup-plugin-node-resolve

--- packages/octicons-vue/lib/main.js
+++ packages/octicons-vue/lib/main.js
@@ -5,7 +5,35 @@ Object.defineProperty(exports, '__esModule', { value: true });
 var Octicon = require('./Octicon');
 var octiconsModular = require('octicons-modular');
 
+function styleInject(css, ref) {
+  if ( ref === void 0 ) { ref = {}; }
+  var insertAt = ref.insertAt;
 
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css = ".octicon{display:inline-block;vertical-align:text-top;fill:currentColor}";
+styleInject(css);
 
 Object.keys(octiconsModular).forEach(function (k) {
   if (k !== 'default') Object.defineProperty(exports, k, {
